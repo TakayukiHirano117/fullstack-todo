65cb80f23f04c9f143cf907ad4b5b1e4166ad438
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-05 18:50:31 +0900

    ✨ Todo一覧apiの実装

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/lib/prismaClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const allBBSPosts = await prisma.todos.findMany();
+
+  return NextResponse.json(allBBSPosts);
+}
+
+export async function POST(req: NextRequest) {
+  const { title, content, due_date } = await req.json();
+
+  const post = await prisma.todos.create({
+    data: {
+      title,
+      content,
+      due_date,
+    },
+  });
+
+  return NextResponse.json(post);
+}
