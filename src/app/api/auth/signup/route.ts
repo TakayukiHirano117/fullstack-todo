3544aed7812df2c3225a8820debf3a4b0747a9ed
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-08 14:03:45 +0900

    ✨サインアップ機能実装！！！！！！！！！！！

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const { email, password } = await req.json();
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+
+  return NextResponse.json({ user: data.user });
+};
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,84 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+export const formSchema = z.object({
+  email: z.string()
+    .email({ message: 'メールアドレスの形式が正しくありません' }),
+  password: z.string()
+    .min(8, { message: 'パスワードは8文字以上で入力してください' }),
+})
 
 const Signup = () => {
+
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      await fetch("http://localhost:3000/api/auth/signup/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+      router.push("/signin")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  
   return (
-    <div>Signup</div>
+    <div className='p-8'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>メールアドレス</FormLabel>
+                <FormControl>
+                  <Input placeholder="メールアドレス" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>パスワード</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="パスワード" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
   )
 }
 
