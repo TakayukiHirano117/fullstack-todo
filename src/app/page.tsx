1ee190e44234602c668aca4052ab9769780fdcb9
TakayukiHirano117 <ikusa.hirano@icloud.com>
2024-12-15 13:30:33 +0900

    ♻️ 余計なコードの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
-// import { FaArrowLeft } from "react-icons/fa";
-
 export default function Home() {
-
 
   return (
     <>
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-6xl font-bold my-8 mr-12">Fullstack Todo</h1>
         <Image src={"project-icon.svg"} alt="" width={400} height={400}></Image>
-        {/* <div className="flex gap-2 mt-8">
-          <FaArrowLeft className="mt-1"/>
-          <p className="">サイドバーからTodoの作成を始めよう</p>
-        </div> */}
       </div>
     </>
   );
